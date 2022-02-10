27ae574f5736ef38602e5733d1c23371b1ed3aa5
soft-insight <j0108rodriguez@gmail.com>
2022-02-10 18:40:46 -0500

    learning C

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <unistd.h>
 /**
  * main - Size of data types
  * print size of types
@@ -6,7 +7,7 @@
  */
 int main(void)
 {
-	fprintf(stderr,\
+	dprintf(STDERR_FILEEDNO,\
 		"and that piece of art isuseful\" - Dora Korpar, 2015-10-19\n");
 	return (1);
 }
