f9d6eb47b5928dc7c77015bab08fbf6c21207e82
soft-insight <j0108rodriguez@gmail.com>
2022-02-10 19:31:39 -0500

    learning C

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -9,7 +9,7 @@
  */
 int main(void)
 {
-	char mss[]="and that piece of art is useful\" - Dora Korpar, 2015-10-19"
-	fprintf(stderr, "mss\n");
+	char ms[]="and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+	fprintf(stderr, "ms\n");
 	return (1);
 }
