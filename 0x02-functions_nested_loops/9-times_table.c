e4496739c962b12dfd9143ac329100c8d9445850
soft-insight <j0108rodriguez@gmail.com>
2022-02-15 16:31:54 -0500

    learning C

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -12,34 +12,30 @@ void times_table(void)
 		for (j = 0; j <= 9; j++)
 		{
 			k = i * j;
-			if (j != 9)
+			if (k == 0 && k < 10)
 			{
-				if (k / 10 == 0)
-				{
-					_putchar(k % 10 + '0');
-					_putchar(',');
-					_putchar(' ');
-					_putchar(' ');
-				}
-				else
-				{
-					_putchar(k / 10 + '0');
-					_putchar(k % 10 + '0');
-					_putchar(',');
-					_putchar(' ');
-				}
+				_putchar(' ');
+				_putchar((k) + '0');
+			}
+			else if (i == 0 && j != 0)
+			{
+				_putchar(' ');
+				_putchar((k) + '0');
+			}
+			else if (k < 10)
+			{
+				_putchar(' ');
+				_putchar((k) + '0');
 			}
 			else
 			{
-				if (k / 10 == 0)
-				{
-					_putchar(k % 10 + '0');
-				}
-				else
-				{
-				_putchar(k / 10 + '0');
-				_putchar(k % 10 + '0');
-				}
+				_putchar(((k) / 10) + '0');
+				_putchar(((k) % 10) + '0');
+			}
+			if (j < 9)
+			{
+				_putchar(',');
+				_putchar(' ');
 			}
 		}
 		_putchar('\n');
