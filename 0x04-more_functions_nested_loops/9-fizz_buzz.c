1ec07e4ca54bea7e6898ed248016dd45317dbc7a
soft-insight <j0108rodriguez@gmail.com>
2022-02-17 10:57:37 -0500

    learning C

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,18 +1,29 @@
 #include <stdio.h>
 #include "main.h"
 
+/**
+ * main - multiples of 3, 5 y 15
+ *
+ * Return: 0
+ */
+
 int main(void)
 {
 	int i;
-	int j;
+	char name1[] = "Fizz";
+	char name2[] = "Buzz";
+	char name3[] = "FizzBuzz";
 
 	for (i = 1; i <= 100; i++)
 	{
-		j = 3 * i;
-
-		if (j < 100)
-			printf("%d ", j);
-
+		if (i % 3 == 0)
+			printf("%s ", name1);
+		else if (i % 5 == 0)
+			printf("%s ", name2);
+		else if (i % 15 == 0)
+			printf("%s ", name3);
+		else
+			printf("%d ", i);
 	}
 	putchar('\n');
 
