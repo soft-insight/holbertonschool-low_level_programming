f53cd456ef09d773e6f1acaf111f8012af8b66c4
soft-insight <j0108rodriguez@gmail.com>
2022-02-21 17:09:55 -0500

    pointers and arrays

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,12 +1,17 @@
 #include "main.h"
 
+/**
+ * rev_string - rev string
+ * @s: input a string
+ * Return: void
+ */
 
 void rev_string(char *s)
 {
 	int b = 0;
 	int i, temp;
 
-	while(s[b] != '\0')
+	while (s[b] != '\0')
 		b++;
 
 	for (i = b - 1; i >= b / 2; i--)
