e8da49441d51bb48ff9a5dd3a65ddb5ecb6348a3
soft-insight <j0108rodriguez@gmail.com>
2022-02-24 16:56:22 -0500

    pointers and arrays

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -3,27 +3,27 @@
 char *_strncat(char *dest, char *src, int n)
 {
 	int i;
-        int j;
-        int k;
+	int j;
+	int k;
 
-        i = 0;
-        j = 0;
-        k = 0;
+	i = 0;
+	j = 0;
+	k = 0;
 
-        while(dest[i] != '\0')
-                i++;
+	while (dest[i] != '\0')
+		i++;
 
-        while(src[j] != '\0')
-                j++;
+	while (src[j] != '\0')
+		j++;
 
-        for (k = 0; k < i + n; k++)
-        {
-                if (k < i)
-                        dest[k] = dest[k];
-                else if (k - i < j)
-                        dest[k] = src[k - i];
+	for (k = 0; k < i + n; k++)
+	{
+		if (k < i)
+			dest[k] = dest[k];
+		else if (k - i < j)
+			dest[k] = src[k - i];
 		else
 			break;
-        }
-        return(dest);
+	}
+	return (dest);
 }
