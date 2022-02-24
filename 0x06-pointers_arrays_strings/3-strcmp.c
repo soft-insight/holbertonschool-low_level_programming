7ae199d64424e1dd66cf19d9ba93f15967eaed68
soft-insight <j0108rodriguez@gmail.com>
2022-02-24 18:58:23 -0500

    pointers and arrays

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -11,13 +11,12 @@
 int _strcmp(char *s1, char *s2)
 {
 	int i = 0;
-	int j = 0;
-	int k;
+	int k = 0;
 
-	while (s1[i] != '\0')
+	while (s1[i] != '\0' && s2[i] != '\0' && k == 0)
+	{
+		k = s1[i] - s2[i];
 		i++;
-	while (s2[j] != '\0')
-		j++;
-	k = i - j;
+	}
 	return (k);
 }
