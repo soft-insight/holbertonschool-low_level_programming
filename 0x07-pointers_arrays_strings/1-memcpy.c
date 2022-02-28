5165b4c697882e3e988d2b195932a3632083ec16
soft-insight <j0108rodriguez@gmail.com>
2022-02-28 15:44:15 -0500

    learning C

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,11 +1,19 @@
 #include "main.h"
 
+/**
+ * _memcpy - copies mem area form src to dest
+ * @dest: destine string
+ * @src: string source
+ * @n: number of bytes form src to dest
+ * Return: dest string
+ */
+
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	int i;
 	unsigned int j;
 
-	while(i != '\0')
+	while (i != '\0')
 		i++;
 
 	for (j = 0; j < n; j++)
