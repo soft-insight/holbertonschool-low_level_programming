0cb36aa88f41a16afad2e0cf724a878c60789504
soft-insight <j0108rodriguez@gmail.com>
2022-03-02 14:05:07 -0500

    learning C

---
+++ 0x08-recursion/README.md
@@ -0,0 +1,18 @@
+# Recursion on C
+
+## Prints a string
+
+## Prints a string in reverse
+
+## Length of a string
+
+## Factorial
+
+## power funtion
+
+## sqrt funtion
+
+## prime number
+
+
+
---
+++ 0x08-recursion/main.h
@@ -0,0 +1,21 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/*
+ * If a header file happens to be included twice,
+ * the compiler will process its contents twice and it
+ * will result in an error. The standard way to prevent
+ * this is to enclose the entire real contents of
+ * the file in a conditional, like this −
+ */
+
+/*
+ * The functions included must prototype for this exercise
+ */
+
+
+int _putchar(char);
+void _puts_recursion(char *s);
+
+
+#endif
