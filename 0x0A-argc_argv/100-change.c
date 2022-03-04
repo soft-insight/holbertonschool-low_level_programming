e1e4209f1f133219b5238fc19f81da7c3d8500b2
soft-insight <j0108rodriguez@gmail.com>
2022-03-04 12:12:50 -0500

    argc argv

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - prints the multiplication
+ * @argc: int of the number of arg
+ * @argv: string of the arguments names
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+    
+    return (0);
+}
