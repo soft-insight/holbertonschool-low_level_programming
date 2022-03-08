92117955e2404f61193a93ac68125ec7cbbfc2fb
soft-insight <j0108rodriguez@gmail.com>
2022-03-08 18:53:55 -0500

    malloc-free

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -4,6 +4,8 @@
 int _putchar(char);
 char *create_array(unsigned int size, char c);
 char*_strdup(char *str); 
-char*str_concat(char *s1, char *s2); 
+char*str_concat(char *s1, char *s2);
+int **alloc_grid(int width, int height);
+
 
 #endif
