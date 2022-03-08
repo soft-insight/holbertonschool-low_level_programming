f7ca9ae770c2d01fc5b0504a5a03de5fe049c2d4
soft-insight <j0108rodriguez@gmail.com>
2022-03-08 14:24:13 -0500

    malloc-free

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -18,9 +18,9 @@ char *str_concat(char *s1, char *s2)
 	j = 0;
 
 	if (s1 == NULL)
-		return (NULL);
-	if (s2 == NULL)
-		return (NULL);
+		s1 = "";
+	if (s1 == NULL)
+		s2 = "";
 
 	while (s1[i])
 		i++;
