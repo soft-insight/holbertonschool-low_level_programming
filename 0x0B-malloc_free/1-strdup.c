2b52279d149f9444be1287b956bec173ac401077
soft-insight <j0108rodriguez@gmail.com>
2022-03-08 10:48:08 -0500

    malloc-free

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -2,10 +2,10 @@
 #include <stdlib.h>
 
 /**
+ * _strdup - copy string
+ * @str: pointer to str
  *
- *
- *
- *
+ * Return: strcp
  */
 
 char *_strdup(char *str)
@@ -14,13 +14,13 @@ char *_strdup(char *str)
 	int i, j;
 
 	j = 0;
-	while( str[j] != '\0')
+	while (str[j] != '\0')
 		j++;
 
 	strcp = malloc(j * sizeof(char));
 
 	for (i = 0; i < j; i++)
 		strcp[i] = str[i];
-	
-	return(strcp);
+
+	return (strcp);
 }
