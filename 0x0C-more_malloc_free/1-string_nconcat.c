253677b75151775c9867f2c0300269b654bd9c51
soft-insight <j0108rodriguez@gmail.com>
2022-03-11 11:44:47 -0500

    malloc-free

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -20,9 +20,9 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	j = 0;
 
 	if (s1 == NULL)
-		s1 = "";
+		s1 = NULL;
 	if (s2 == NULL)
-		s2 = "";
+		s2 = NULL;
 	while (s1[i])
 		i++;
 	while (s2[j])
