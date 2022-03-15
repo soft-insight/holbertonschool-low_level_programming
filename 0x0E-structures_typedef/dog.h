96599c447d518c9c2c61c9aea63b1f28d414e68f
soft-insight <j0108rodriguez@gmail.com>
2022-03-15 17:19:42 -0500

    structures

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -10,12 +10,13 @@
  * Description: struct dog
  */
 
-typedef struct dog
+struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-} dog_t;
+}; 
+typedef struct dog dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
