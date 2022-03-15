53ef406565b1fc7fd305b3af180c468115ed974b
soft-insight <j0108rodriguez@gmail.com>
2022-03-15 08:54:11 -0500

    structures and typedef

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,18 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ *
+ *
+ *
+ *
+ */
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	new_dog->name = name;
+	new_dog->age = age;
+	new_dog->owner = owner;
+
+	return(new_dog);
+}
