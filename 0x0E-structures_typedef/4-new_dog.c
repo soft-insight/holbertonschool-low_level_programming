5f02a7425f4c93b8bdf688d38b406b155056ab72
soft-insight <j0108rodriguez@gmail.com>
2022-03-15 16:38:04 -0500

    structures and typedef

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -27,32 +27,34 @@ int _slen(char *s)
 
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	dog_t *new_dog; /**/
+	dog_t *little_dog; /**/
 	int i;
 	int len_name = _slen(name);
 	int len_owner = _slen(owner);
 
-	new_dog = malloc(sizeof(dog_t));
-	if (new_dog == NULL)
+	little_dog = malloc(sizeof(dog_t));
+	if (little_dog == NULL)
 		return (NULL);
-	new_dog->name = malloc(len_name + 1);
-	if (new_dog->name == NULL)
+	little_dog->name = malloc(len_name + 1);
+	if (little_dog->name == NULL)
 	{
-		free(new_dog);
+		free(little_dog);
 		return (NULL);
 	}
 	for (i = 0; i < len_name; i++)
-		new_dog->name[i] = name[i];
-	new_dog->age = age;
-	new_dog->owner = malloc(len_owner + 1);
-	if (new_dog->owner == NULL)
+		little_dog->name[i] = name[i];
+
+	little_dog->age = age;
+	/* */
+	little_dog->owner = malloc(len_owner + 1);
+	if (little_dog->owner == NULL)
 	{
-		free(new_dog->name);
-		free(new_dog);
+		free(little_dog->name);
+		free(little_dog);
 		return (NULL);
 	}
 
 	for (i = 0; i < len_owner; i++)
-		new_dog->owner[i] = owner[i];
-	return (new_dog);
+		little_dog->owner[i] = owner[i];
+	return (little_dog);
 }
