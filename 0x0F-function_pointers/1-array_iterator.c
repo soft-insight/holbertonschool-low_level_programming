2dacb1ea7c3983a30114daac8e38f079429964c8
soft-insight <j0108rodriguez@gmail.com>
2022-03-17 14:35:12 -0500

    pointers to functions

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -14,7 +14,7 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	unsigned long int i;
 	
-	if(array)
+	if(array != NULL || action != NULL)
 	{
 		for (i = 0; i < size; i++)
 			action(array[i]);
