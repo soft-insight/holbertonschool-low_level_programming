c2e64b4b5f6e11b34560d69581956d73a2e89394
soft-insight <j0108rodriguez@gmail.com>
2022-03-17 18:31:49 -0500

    pointers to functions

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,28 @@
+#include "function_pointers.h"
+#include <stdlib.h>
+
+/**
+ * int_index - returns the index of success when comparing
+ * @array: the array
+ * @size: the size of array
+ * @cmp: the funct to compare that returns 0 when not success
+ *
+ * Return: the index i of succees or -1
+ */
+
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (array != NULL && cmp != NULL)
+	{
+		for (i = 0; i < size; i++)
+		{
+			if (cmp(array[i]) != 0)
+				return (i);
+		}
+	return (-1);
+}
+
+
