c02077fcbb94afb6125e47eb87b2fbb4c5ab4f84
soft-insight <j0108rodriguez@gmail.com>
2022-03-19 21:12:50 -0500

    Variadic Functions

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdarg.h>
+#include "variadic_functions.h"
+
+/**
+ * sum_them_all - check the code for Holberton School students.
+ * @n: Is the number of total addends
+ * Return: Always 0.
+ */
+int sum_them_all(const unsigned int n, ...)
+{
+	va_list arguments;
+	int sum = 0;
+	unsigned int i;
+
+	if (n == 0)
+		return (0);
+
+	va_start(arguments, n);
+
+	for (i = 0; i < n; i++)
+		sum += va_arg(arguments, int);
+
+	va_end(arguments);
+	return (sum);
+}
+
