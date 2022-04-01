0262d35c872aac8bfd35a95260868040f693e112
soft-insight <j0108rodriguez@gmail.com>
2022-04-01 12:45:29 -0500

    length of a list

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -11,12 +11,12 @@
 
 size_t listint_len(const listint_t *h)
 {
-        size_t i = 0;
+	size_t i = 0;
 
-        while (h != NULL)
-        {
-                h = h->next;
-                i++;
-        }
-        return (i);
+	while (h != NULL)
+	{
+		h = h->next;
+		i++;
+	}
+	return (i);
 }
