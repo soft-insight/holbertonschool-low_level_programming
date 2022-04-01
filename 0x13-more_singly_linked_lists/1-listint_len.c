445fe389915f2f1dde9612cb12cb9b3f212c5be5
soft-insight <j0108rodriguez@gmail.com>
2022-04-01 12:42:59 -0500

    length of a list

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * listint_len - print the length of a linked list
+ * @h: the head of the list
+ *
+ * Return: the number of element, the lenght
+ */
+
+size_t listint_len(const listint_t *h)
+{
+        size_t i = 0;
+
+        while (h != NULL)
+        {
+                h = h->next;
+                i++;
+        }
+        return (i);
+}
