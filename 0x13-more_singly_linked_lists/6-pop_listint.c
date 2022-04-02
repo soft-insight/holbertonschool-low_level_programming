710262d9cf6ad69bd9289164bb44f9146d6e4e57
soft-insight <j0108rodriguez@gmail.com>
2022-04-02 12:52:45 -0500

    pop without free ;)

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ *
+ *
+ *
+ */
+
+int pop_listint(listint_t **head)
+{
+	listint_t *temp;
+	int n;
+
+	if (head == NULL)
+		return (0);
+
+	n = (*head)->n;
+	temp = (*head)->next;
+	/* free(*head); */
+	*head = temp;
+
+	return (n);
+}
+
+
+
