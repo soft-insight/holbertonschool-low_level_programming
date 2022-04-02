69ded26ea1a0d9fe140201eb2ef169bd1ab92dea
soft-insight <j0108rodriguez@gmail.com>
2022-04-02 13:46:57 -0500

    sum the list

---
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * sum_listint - sum a list of ints
+ * @head: the head of the list
+ * 
+ * Return: the sum
+ */
+
+int sum_listint(listint_t *head)
+{
+	int sum = 0;
+
+	if (head == NULL)
+		return (0);
+
+	while (head != NULL)
+	{
+		sum = sum + head->n;
+		head = head->next;
+	}
+	return (sum);
+}
