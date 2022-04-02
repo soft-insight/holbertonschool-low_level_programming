2e2aa39ef172907459569cc23eb8619432475490
soft-insight <j0108rodriguez@gmail.com>
2022-04-02 13:36:51 -0500

    get node

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -3,9 +3,11 @@
 #include "lists.h"
 
 /**
+ * get_nodeint_at_index - get node
+ * @head: the head of linked list
+ * @index: the index to get
  *
- *
- *
+ * Return: the node at the index
  */
 
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
@@ -20,7 +22,7 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 		i++;
 	}
 	if (index > i)
-		return NULL;
+		return (NULL);
 
 	return (head);
 }
