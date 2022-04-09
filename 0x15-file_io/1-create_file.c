422b4de27414961c8d5796924b80d7f186d20509
soft-insight <j0108rodriguez@gmail.com>
2022-04-08 20:35:19 -0500

    fix existence

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -13,14 +13,11 @@ int create_file(const char *filename, char *text_content)
 	int i = 0;
 	int fd, wr;
 
-	if (filename == NULL)
+	if (!filename)
 		return (-1);
 
-	while(text_content[i] != '\0')
+	while (text_content[i] != '\0')
 		i++;
-
-	/* if (!filename) */
-	/* 	return (-1); */
 
 	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
 	if (fd == -1)
@@ -28,7 +25,7 @@ int create_file(const char *filename, char *text_content)
 
 	if (text_content)
 		wr = write(fd, text_content, i);
-	
+
 	if (wr == -1)
 		return (-1);
 	close(fd);
