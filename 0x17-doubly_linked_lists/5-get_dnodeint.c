429f1f1a5aed1d8283bcd013c48275f1a8aab6ff
soft-insight <j0108rodriguez@gmail.com>
2022-05-12 19:37:57 -0500

    get node at idx

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -11,7 +11,8 @@
 dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 {
 	unsigned int i = 0;
-
+	if (head == NULL)
+		return (NULL);
 	while (head != NULL)
 	{
 		if (i == index)
