3fd9ba2d97405f17b477e66682b63ac8998dcf3e
soft-insight <j0108rodriguez@gmail.com>
2022-05-12 21:12:19 -0500

    add node at idx

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -12,22 +12,17 @@
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
 	dlistint_t *new_mem, *temp = *h;
-	unsigned int i = 0, j = 0;
+	/* unsigned int counter = 0 */
 
-	while (*h != NULL)
-	{
-		(*h) = (*h)->next;
-		j++;
-	}
-	
-	if (idx > j + 1)
-		return (NULL);
 	if (idx == 0)
 		return (add_dnodeint(h, n));
 
-	if (temp)
-		for (i = 0; temp->next != NULL && i < idx - 1; i++)
-			temp = temp->next;
+	for (; idx != 1; idx--)
+	{
+		if (temp == NULL)
+			return (NULL);
+		temp = temp->next;
+	}
 
 	if (temp->next == NULL)
 		return (add_dnodeint_end(h, n));
