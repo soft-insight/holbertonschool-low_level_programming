691b51737ee181210c1e38ac0f3bd01286e0dd0b
soft-insight <j0108rodriguez@gmail.com>
2022-05-12 19:34:15 -0500

    get node at idx

---
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+
+/**
+ * get_dnodeint_at_index - obtain the node of a dll at a index
+ * @head: pointer to head
+ * @index: the index
+ *
+ * Return: the node at index
+ */
+
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+	unsigned int i = 0;
+
+	while (head != NULL)
+	{
+		if (i == index)
+		{
+			head = head->next;
+			return (head);
+		}
+		i++;
+	}
+	return (head);
+}
