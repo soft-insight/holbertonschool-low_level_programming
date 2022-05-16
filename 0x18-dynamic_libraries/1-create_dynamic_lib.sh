4f18c41fb96a19bb332a87ba26165022a2d015a8
soft-insight <j0108rodriguez@gmail.com>
2022-05-16 12:22:03 -0500

    dyn lib

--- 0x18-dynamic_libraries/1-create_dynamic_lib.sh
+++ 0x18-dynamic_libraries/1-create_dynamic_lib.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 gcc -c -fPIC *.c
 gcc -shared -o liball.so *.o
-EXPORT LD_LIBRARY_PATH=.:$LD_LIBRARY_PATH
+export LD_LIBRARY_PATH=.:$LD_LIBRARY_PATH
 #gcc -Wall -pedantic -Werror -Wextra -L . 0-main.c -l all -o len
